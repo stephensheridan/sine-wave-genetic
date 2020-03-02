2ff12abd809fc3f062f0a0a3e1a80781e72967d4
Stephen Sheridan <stesher@gmail.com>
2020-03-02 22:57:46 +0000

    Fixed crossover loop percentage and cleaned up sorting code.

--- Population.java
+++ Population.java
@@ -1,4 +1,4 @@
-// Genetic algorithm for solving a +2 b +3 c +4 d = 30
+// Genetic algorithm for solving sin(x) * sin(0.4f * x) * sin(3.0f * x);
 // S. Sheridan 27/02/2014
 // Institute of Technology Blanchardstown
 
@@ -46,17 +46,14 @@ public class Population{
 	public void buildRouletteWheel(){
         // Build a roulette wheel array for selecting parents
 		rouletteWheelSize = 0;
-        for (int i=0; i < chromosome_length; i++)
-        {
+        for (int i=0; i < chromosome_length; i++){
  	   		rouletteWheelSize += i + 1;
         }
         rouletteWheel = new int[rouletteWheelSize];
         int num_trials = chromosome_length;
         int index = 0;
-        for (int i=0; i<chromosome_length; i++)
-        {
- 		   for (int j=0; j<num_trials; j++)
- 		   {
+        for (int i=0; i<chromosome_length; i++){
+ 		   for (int j=0; j<num_trials; j++){
  			   rouletteWheel[index++] = i;
  		   }
  		   num_trials--;
@@ -87,13 +84,9 @@ public class Population{
 		return population_size;
 	}
 	// Return fitness for given chromosome
-	private float fitness(float x)
-	{
+	private float fitness(float x){
 		float f = (float)(Math.sin(x) * Math.sin(0.4f * x)
 		        * Math.sin(3.0f * x));
-		//if (f < 0)
-		//	System.out.println("*********** Minus Value *********** " + x);
-
 		return f;
 	}
 	
@@ -104,19 +97,17 @@ public class Population{
 		population[i] = population[k];
 		population[k] = temp;
 	}
-	// Sort the population based on their fitness ASCENDING (lower number better fitness)
+	// Sort the population based on their fitness (highest to lowesr)
 	public void sort(){
-		int i, j, first, temp;  
-	     for ( i = population_size - 1; i > 0; i -- )  
-	     {
-	          first = 0;   //initialize to subscript of first element
-	          for(j = 1; j <= i; j ++)   //locate smallest element between positions 1 and i.
-	          {
-	               if( population[j].getFitness() < population[first].getFitness() )         
-	                 first = j;
-	          }
-	          swap(first, i);
-	      } 
+		// Selection sort - Ooops should do better
+		for(int i = 0; i < population_size; i++){
+			int k = i;
+			for(int j = i + 1; j < population_size; j++){
+				if (population[j].getFitness() > population[k].getFitness())
+					k = j;
+			}
+			swap(k, i);
+		}
 	}
 	
  	// Set the fitness function for each populaiton member
@@ -128,21 +119,19 @@ public class Population{
  	}
 	
 	// Select and combine genetic material
-	public void crossover()
-    {
-       int num = (int)(population_size * crossoverFraction);
-	   for (int i=num; i < population_size; i++)
-       {
+	public void crossover(){
+       int num = (int)(population_size-(population_size * crossoverFraction));
+	   // Bottom % of the population will be effected by crossover
+	   for (int i=num; i < population_size; i++){
+		  // Select two parents using roulette wheel 	
  		  int c1 = (int)(rouletteWheelSize * Math.random() * 0.9999f);
  		  int c2 = (int)(rouletteWheelSize * Math.random() * 0.9999f);
  		  c1 = rouletteWheel[c1];
  		  c2 = rouletteWheel[c2];
-		  if (c1 != c2)
- 		  {
+		  if (c1 != c2){
  			  // Perform single point crossover
 			  int locus = 1 + (int)((chromosome_length - 2) * Math.random());
-			  for (int g=0; g < chromosome_length; g++)
- 			  {
+			  for (int g=0; g < chromosome_length; g++){
  				  if (g < locus)
  				  	population[i].setGeneAt(g, population[c1].getGeneAt(g)); // Copy from parent c1 up to locus g
  				  else
@@ -152,25 +141,21 @@ public class Population{
        }
      }
 	 // Carry out mutation on population	 
-     public void mutate()
-     {
+     public void mutate(){
 	   int num = (int)(population_size * mutationFraction);
-	   for (int i=0; i<num; i++)
-       {
+	   // Loop through a number of chromosomes to mutate
+	   for (int i=0; i<num; i++){
  		  int c = (int)(population_size * Math.random() * 0.99); 	// Random chromosome
  		  int g = (int)(chromosome_length * Math.random() * 0.99);	// Random gene
 		  population[c].mutateGeneAt(g);
        }
      }
 	 // Remove any duplicate chromosomes from population to prevent stagnation
-     public void removeDuplicates()
-     {
- 		for (int i=population_size - 1; i>3; i--) // i>3 dont touch the top of the population
- 		{
- 			for (int j=0; j<i; j++)
- 			{
- 				if (population[i].equals(population[j]))
- 				{
+     public void removeDuplicates(){
+ 		for (int i=population_size - 1; i>3; i--){
+			// i>3 dont touch the top of the population
+ 			for (int j=0; j<i; j++){
+ 				if (population[i].equals(population[j])){
  					int g = (int)(chromosome_length * Math.random() * 0.99);
 					population[i].mutateGeneAt(g);
  					break;
@@ -179,13 +164,11 @@ public class Population{
  		}
      }
 	 // Called to envoke one complete evolution		 
-     public void evolve()
-     {
+     public void evolve(){
   	   evaluate();
   	   sort();
   	   crossover();
   	   mutate();
   	   removeDuplicates();
      }
-	
-}
+ }
